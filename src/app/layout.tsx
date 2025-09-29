9f4478b1747d4d7ceee079d56a4eca86a7063a4e
d4v3w <mail@d4v3w.com>
2025-09-29 17:40:03 +0100

    Update lang en-GB

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en-GB" className={`${geist.variable}`}>
       <body className="bg-black text-white">{children}</body>
     </html>
   );
