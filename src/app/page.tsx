b196cac414ceb40571ee2b033b4045dc6aa8d1a9
Dave Williams <eretyz@hotmail.com>
2025-10-14 19:59:54 +0100

    Update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,7 @@ const Shows = () => (
     <section className="mt-3 columns-1 gap-3 sm:columns-2 lg:columns-3">
       <img
         src="https://xnrw2k7p6j.ufs.sh/f/kor843t3OqX1jhiX7K9CW63sKRVJPxiQH09w81nhzYZI5bMg"
-        alt="Freaks and Geeks - Stratford, London, November 21st 2025"
+        alt="The Custard Screams at Freaks and Geeks - Stratford, London, November 21st 2025"
         className="mb-3 w-full rounded-lg object-cover"
       />
     <h2 className="text-xl font-bold text-amber-400">Previous shows</h2>
@@ -108,12 +108,12 @@ const Shows = () => (
     <section className="mt-3 columns-1 gap-3 sm:columns-2 lg:columns-3">
       <img
         src="https://xnrw2k7p6j.ufs.sh/f/kor843t3OqX12Faf3d4f6C7O5UiyzsSR8NkawKYFJxpQXubM"
-        alt="Freaks and Geeks - Stratford, London, May 31st 2025"
+        alt="The Custard Screams at Freaks and Geeks - Stratford, London, May 31st 2025"
         className="mb-3 w-full rounded-lg object-cover"
       />
       <img
         src="https://xnrw2k7p6j.ufs.sh/f/kor843t3OqX11TVvcM2zyFK0ROkwl3WSGv8XHuon7LfdbVrj"
-        alt="The Custard Screams"
+        alt="The Custard Screams Skull Logo"
       />
     </section>
   </article>
